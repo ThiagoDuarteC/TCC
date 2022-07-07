2fa3dd48fcbf77946645c021a48c8c573a69037e
ThiagoDuarteC <thiagodcorreia01@gmail.com>
2022-07-07 14:48:46 -0300

    Home Page

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Freelers</title>
+    <link rel="stylesheet" href="css/style1.css">
+</head>
+<body>
+
+        <header class="logo"></header><!--logo-->
+        <div class="clear"></div><!--clear-->
+
+        <nav>
+            <h2>Nos mostre seu trabalho!</h2>
+            <p>Estamos aqui com o objetivo de ajudar trabalhadores independentes <br>a encontrar trabalho de maneira mais fácil, simples e rápida.
+            </p>
+            <div class="button">
+                <a href="cadastro.php">Crie seu Perfil</a>
+            <div class="button2">
+                <a href="main_page.php">Entrar no Site</a>
+            </div><!--button2-->
+        </nav>
+        <div class="clear"></div><!--clear-->
+
+</body>
+</html>
